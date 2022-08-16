18c7570a27c6b463e9a33c305a390cff2992169a
Miguel Arcanjo <945507756m@gmail.com>
2022-08-16 19:01:30 -0300

    fist commit

---
+++ src/main/java/com/mycompany/dadospessoais/DadosPessoais.java
@@ -0,0 +1,37 @@
+package com.mycompany.dadospessoais;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Miguel Arcanjo
+ * Com o intuito de Estudar mais sobre a classe JOptionPane
+ */
+public class DadosPessoais {
+
+    public static void main(String[] args) {
+            
+    // variavel
+    String nome;
+    int idade;
+    double peso;
+    String estado;
+    
+    
+    // entrada de dados
+    nome = JOptionPane.showInputDialog(null, "Insira o seu nome: ", "Digite seu nome e sobrenome", JOptionPane.PLAIN_MESSAGE);
+    idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira o sua idade: ", "Digite sua idade", JOptionPane.PLAIN_MESSAGE));
+    peso = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o seu peso: ", "Digite seu peso usando .(ponto) como divisor", JOptionPane.PLAIN_MESSAGE));        
+    estado = JOptionPane.showInputDialog(null, "Insira o seu estado: ", "Digite o nome do estado que você esta atualmente", JOptionPane.PLAIN_MESSAGE);
+    
+    // saida de dados
+    JOptionPane.showMessageDialog(null, "seu nome é: " + nome, "Saída de dados separados", JOptionPane.PLAIN_MESSAGE);
+    JOptionPane.showMessageDialog(null, "sua idade é: " + idade, "Saída de dados separados", JOptionPane.PLAIN_MESSAGE);
+    JOptionPane.showMessageDialog(null, "seu peso é: " + peso, "Saída de dados separados", JOptionPane.PLAIN_MESSAGE);
+    JOptionPane.showMessageDialog(null, "seu estado é: " + estado, "Saída de dados separados", JOptionPane.PLAIN_MESSAGE);
+    
+    // saida de dados separados
+    JOptionPane.showMessageDialog(null, "Nome: " + nome + " | Idade: " + idade + " | Peso: " + peso + " | Estado: " + estado, "Todos os dados juntos", JOptionPane.PLAIN_MESSAGE);
+    
+    
+    }
+}
